9cf1ae2de2a9e9c878da31d7e00e672c812aa33c
Amrindra Ouk <amrindraouk@Amrindras-MacBook-Pro.local>
2022-05-13 16:12:58 -0400

    added routes to handle request for product routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 
 //Have to config dotenv in order to use it
 dotenv.config();
@@ -20,6 +21,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on ${process.env.PORT}`);
--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,107 @@
+//Using Router function from express
 const router = require("express").Router();
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+// const CryptoJS = require("crypto-js");
+const Product = require("../models/Product");
+
+//CREATE PRODUCT and ONLY ADMIN CAN CREATE PRODUCT
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Update Product
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        //Take everything inside req.body and then set/update them
+        $set: req.body,
+      },
+      //To make it work we have to add new:true
+      { new: true }
+    );
+
+    //Sending updated response back to user if there is no any errors
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//DELETE PRODUCT
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted!");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET SINGLE PRODUCT
+//In this case, users, admin, and guests can find products, so we don't have to pass "veryfyToken" middleware
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL USERS
+// router.get("/", verifyTokenAndAdmin, async (req, res) => {
+//   //using query to limit how many users we want to return back
+//   const query = req.query.new;
+
+//   try {
+//     const users = query
+//       ? await User.find().sort({ _id: -1 }).limit(5)
+//       : await User.find();
+//     res.status(200).json(users);
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
+
+//USER STATS
+//Using this stat to return total users per month. EX: number of users have registered....
+// router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+//   const date = new Date();
+//   const lastyear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+//   try {
+//     const data = await User.aggregate([
+//       { $match: { createdAt: { $gte: lastyear } } },
+//       {
+//         $project: {
+//           //take only month from createdAt in database then store it in month variable
+//           month: { $month: "$createdAt" },
+//         },
+//       },
+//       {
+//         $group: {
+//           _id: "$month",
+//           total: { $sum: 1 },
+//         },
+//       },
+//     ]);
+//     res.status(200).json(data);
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
 
 module.exports = router;
