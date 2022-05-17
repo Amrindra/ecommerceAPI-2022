03e93272f178f8d1bdb0caf004f894d53a7709c8
Amrindra Ouk <amrindraouk@Amrindras-MacBook-Pro.local>
2022-05-17 15:37:41 -0400

    installed cors dependency and added stipe route in index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 //Have to config dotenv in order to use it
 dotenv.config();
@@ -18,6 +20,8 @@ mongoose
     console.log(error);
   });
 
+app.use(cors);
+
 //if we don't use express.json we won't be able to send post request
 app.use(express.json());
 
@@ -26,6 +30,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/stripe", stripeRoute);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on ${process.env.PORT}`);
